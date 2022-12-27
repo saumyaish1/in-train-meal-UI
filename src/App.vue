4ddcc7636128990fa30203fa309cdab6651bd9f6
Saumya Tripathi <saumyaishatripathi@gmail.com>
2022-12-27 20:54:39 +0530

    meal-delivery app frontend setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainMenu />
+    <HelloWorld msg="Welcome to the vue app home"/>
+    
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MainMenu from './components/nav.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MainMenu
   }
 }
 </script>
