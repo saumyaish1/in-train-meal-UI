29e7e5050944646885c2d7e9f4c6595e99d08c72
Saumya Tripathi <saumyaishatripathi@gmail.com>
2022-12-27 22:05:32 +0530

    host configuration added

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  "devServer": {
+    "public": "https://in-train-meal-ui.onrender.com/"
+  }
 })
